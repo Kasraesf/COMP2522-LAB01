408283c1956fb4afe114e9aa4e1855cfb5b61e13
Kasraesf <kasraesf1383@gmail.com>
2022-09-10 22:47:25 -0700

    Added a Main class and a main mehtod and added a message about my favourite soccer team.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Main Class.
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("My favourite Soccer team is Real Madrid.");
+  }
+}
